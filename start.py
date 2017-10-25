250f5e43ed6c14f4c6839c8f6f8b7da0d4fb7847
Hanjara Cahya Adhyatma <mangatkk@gmail.com>
2017-10-25 21:32:44 +0700

    ws

--- start.py
+++ start.py
@@ -9,6 +9,7 @@ os.system("pdflatex.exe -synctex=1 -interaction=nonstopmode \"" + file +"\".tex"
 print("\n###############################################################################")
 print("#                                    FINISH                                   #")
 print("###############################################################################")
+print("DELETE del *.aux *.log *.out *.lot *.lof *.txt *.toc *.gz")
 os.system("del *.aux *.log *.out *.lot *.lof *.txt *.toc *.gz")
 print("\n###############################################################################")
 print("#                               CLEANUP DONE                                  #")
